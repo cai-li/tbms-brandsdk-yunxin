1e9e282ec962577ca413c49734f0f4032f0cac6d
罗嗣 <fangbo.fb@alibaba-inc.com>
2019-02-18 15:05:09 +0800

    update version

---
+++ packages/tbms-brandsdk-yunxin/src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * ----------------------------------
+ * @file index.ts
+ * @desc branksdk, tbms, yunxin
+ * @author tbms-brandsdk-yunxin
+ * ----------------------------------
+ */
+
+import SDK from 'tbms-sdk';
+
+export default class extends SDK {
+  constructor() {
+    super()
+  }
+}
